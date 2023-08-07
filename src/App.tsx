b1ca13d46b3d29f67561d3da8525f4430f8633b1
DiamadDev <ysw991106@gmail.com>
2023-08-07 16:45:16 +0900

    Revert "feat: pdf 파일 편집 기능을 사용하기 위해서 React.Context 사용"
    This reverts commit 701520f226d2588491a819274cf9a2512b034df4.

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,17 @@ import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./components/Home";
 import Work from "./components/Work";
-import {PdfContextProvider} from "./components/PdfContext";
 
 function App() {
     return (
-        <PdfContextProvider>
-            <div className="App">
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<Home/>}/>
-                        <Route path="/work" element={<Work/>}/>
-                    </Routes>
-                </BrowserRouter>
-            </div>
-        </PdfContextProvider>
-
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/work" element={<Work/>}/>
+                </Routes>
+            </BrowserRouter>
+        </div>
     );
 }
 
