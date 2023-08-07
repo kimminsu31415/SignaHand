701520f226d2588491a819274cf9a2512b034df4
DiamadDev <ysw991106@gmail.com>
2023-08-07 15:25:55 +0900

    feat: pdf 파일 편집 기능을 사용하기 위해서 React.Context 사용

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,21 @@ import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./components/Home";
 import Work from "./components/Work";
+import {PdfContextProvider} from "./components/PdfContext";
 
 function App() {
     return (
-        <div className="App">
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/work" element={<Work/>}/>
-                </Routes>
-            </BrowserRouter>
-        </div>
+        <PdfContextProvider>
+            <div className="App">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Home/>}/>
+                        <Route path="/work" element={<Work/>}/>
+                    </Routes>
+                </BrowserRouter>
+            </div>
+        </PdfContextProvider>
+
     );
 }
 
